2a8b00badd8e42bdf2dfb9149494f1285db179ec
alumno <jpdefrutos@gmail.com>
2018-10-25 21:04:51 -0300

    Lab03 – Req01 - LISTO

---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/MyReceiver.java
@@ -0,0 +1,12 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
